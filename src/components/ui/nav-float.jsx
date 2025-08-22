148f9387637cc960e066725d24245419d4d0d6a7
Manpreet113 <manpreet10542@gmail.com>
2025-08-22 13:19:25 +0530

    refactor: update background colors in Nav components for improved visual consistency

--- src/components/ui/nav-float.jsx
+++ src/components/ui/nav-float.jsx
@@ -66,7 +66,7 @@ export const NavBody = ({
       }}
       className={cn(
         "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-2 lg:flex dark:bg-transparent",
-        visible && "bg-white/80 dark:bg-neutral-950/80",
+        visible && "bg-white/80 dark:bg-indigo-950/50",
         className
       )}>
       {children}
@@ -98,7 +98,7 @@ export const NavItems = ({
           {hovered === idx && (
             <motion.div
               layoutId="hovered"
-              className="absolute inset-0 h-full w-full rounded-full bg-gray-100 dark:bg-neutral-800" />
+              className="absolute inset-0 h-full w-full rounded-full bg-gray-100 dark:bg-indigo-950" />
           )}
           <span className="relative z-20">{item.name}</span>
         </a>
@@ -132,7 +132,7 @@ export const MobileNav = ({
       }}
       className={cn(
         "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-transparent rounded-lg px-0 py-2 lg:hidden",
-        visible && "bg-white/80 dark:bg-neutral-950/80",
+        visible && "bg-white/80 dark:bg-indigo-950/50",
         className
       )}>
       {children}
@@ -155,8 +155,7 @@ export const MobileNavHeader = ({
 export const MobileNavMenu = ({
   children,
   className,
-  isOpen,
-  onClose
+  isOpen
 }) => {
   return (
     <AnimatePresence>
@@ -166,7 +165,7 @@ export const MobileNavMenu = ({
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           className={cn(
-            "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-neutral-950",
+            "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start backdrop-blur-lg gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-indigo-950/80",
             className
           )}>
           {children}
@@ -191,7 +190,7 @@ export const NavbarLogo = () => {
   return (
     <a
       href="#"
-    //   className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
+      className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       {/* <img
         src="https://assets.aceternity.com/logo-dark.png"
